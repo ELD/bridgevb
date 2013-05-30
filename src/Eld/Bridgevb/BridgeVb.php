f5d51de7e48741cb0dabd8d0ea13eaa6b2ff1067
Eric <eric.dattore@gmail.com>
2013-05-29 23:34:49 -0600

    Added more configuration files and finished fleshing out login. Work needs to continue to prevent uneeded sessions from being added to the database table.

--- src/Eld/Bridgevb/BridgeVb.php
+++ src/Eld/Bridgevb/BridgeVb.php
@@ -8,106 +8,197 @@ use Illuminate\Support\Facades\Cookie;
 
 class BridgeVb {
 
-	// Connection string referring to the database connection specified in database.php
+	// The database connection from the config file
 	protected $connection;
-	// Cookie hash unique to each vBulletin installation
+	// Cookie hash
 	protected $cookieHash;
-	// The prefix of the vBulletin cookies
+	// Cookie prefix
 	protected $cookiePrefix;
-	// The hash that represents the session inside the database
-	protected $sessionHash;
-	// The ID hash
-	protected $idHash;
-	// The user's ID
-	protected $userId;
-	// The last activity of the member
-	protected $lastActive;
-	// The password hash contained in the database
-	protected $dbPass;
+	// Cookie timeout time
+	protected $cookieTimeout;
+	// Database table prefix
+	protected $databasePrefix;
+	// Default user info if none is found
+	protected $defaultUser = array(
+		'userid' => 0,
+		'username' => 'unregistered',
+		'usergroupid' => 3,
+		'membergroupids' => '',
+		'sessionhash' => '',
+		'salt' => ''
+	);
+	// Forum URL
+	protected $forumPath;
+	// The columns to select when fetching user info
+	protected $userColumns;
+	// Array of user groups
+	protected $userGroups;
+	// User info array
+	protected $userInfo;
 
 	public function __construct()
 	{
 		$this->connection = Config::get('bridgevb::connection');
 		$this->cookieHash = Config::get('bridgevb::cookie_hash');
 		$this->cookiePrefix = Config::get('bridgevb::cookie_prefix');
+		$this->cookieTimeout = DB::connection($this->connection)
+			->table('setting')->where('varname', '=', 'cookietimeout')->take(1)
+			->get()[0]->value;
+		$this->databasePrefix = Config::get('bridgevb::db_prefix');
+		$this->forumPath = Config::get('bridgevb::forum_path');
+		$this->userColumns = Config::get('bridgevb::user_columns');
+		$this->userGroups = Config::get('bridgevb::user_groups');
+
+		$this->setUserInfo($this->defaultUser);
+		$this->authenticateSession();
 	}
 
-	public function test()
+	public function is($group) {}
+
+	public function isLoggedIn() 
 	{
-		// dd(Cookie::get($this->cookiePrefix . 'userid'));
-		$this->isLoggedIn();
+		return ($this->userInfo->userid ? true : false);
 	}
 
-	/**
-	 * Authenticates the vBulletin user using cookies and sessions. Returns false if the user isn't logged in.
-	 * @return boolean Returns whether the user is logged in or not.
-	 */
-	public function isLoggedIn()
+	public function isAdmin() {}
+
+	public function getLogoutHash() {}
+
+	protected function authenticateSession()
 	{
-		if(isset($_COOKIE[$this->cookiePrefix . 'userid']) && isset($_COOKIE[$this->cookiePrefix . 'password']))
+		$userid = (isset($_COOKIE[$this->cookiePrefix . 'userid']) ? $_COOKIE[$this->cookiePrefix . 'userid'] : false);
+		$password = (isset($_COOKIE[$this->cookiePrefix . 'password']) ? $_COOKIE[$this->cookiePrefix . 'password'] : false);
+
+		$sessionHash = (isset($_COOKIE[$this->cookiePrefix . 'sessionhash']) ? $_COOKIE[$this->cookiePrefix . 'sessionhash'] : false);
+
+		if($userid && $password)
 		{
-			if($this->getCookie($_COOKIE[$this->cookiePrefix . 'userid'], $_COOKIE[$this->cookiePrefix . 'password']))
+			$user = $this->isValidCookieUser($userid, $password);
+
+			if($user)
 			{
-				return $_COOKIE[$this->cookiePrefix . 'userid'];
+				$sessionHash = $this->createSession($userid);
+			}
+			else
+			{
+				return false;
 			}
 		}
-		if(isset($_COOKIE[$this->cookiePrefix . 'sessionhash']))
+
+		if($sessionHash)
 		{
-			if($this->getSession($_COOKIE[$this->cookiePrefix . 'sessionhash']))
+			$session = DB::connection($this->connection)->table($this->databasePrefix . 'session')->where('sessionhash', '=', $sessionHash)
+				->where('idhash', '=', $this->fetchIdHash())->where('lastactivity', '>', $this->cookieTimeout)->get();
+
+			if($session)
+				$session = $session[0];
+			else
+				return false;
+
+			if($session && ($session->host == Request::server('REMOTE_ADDR')))
 			{
-				return $this->getSession($_COOKIE[$this->cookiePrefix . 'sessionhash']);
-			}
-		}
-		return false;
-	}
+				$userinfo = DB::connection($this->connection)->table($this->databasePrefix . 'session')->where('userid', '=', $session->userid)->take(1)->get();
 
-	public function getUserInfo($id)
-	{
-		$result = DB::connection($this->connection)->table('user')->where('userid', '=', $id)->take(1)->get();
-		return ($result ? $result[0] : false);
-	}
-
-	private function getSession($hash)
-	{
-		$ip = explode('.', Request::server('REMOTE_ADDR'));
-		$ip = implode('.', array_splice($ip, 0, 4 - 1));
-		
-		$newIdHash = md5(Request::server('HTTP_USER_AGENT') . $ip);
-
-		$result = DB::connection($this->connection)->table('session')->where('sessionhash', '=', $hash)
-						->take(1)->get();
-
-		if ($result)
-		{
-			$this->sessionHash = $result[0]->sessionhash;
-			$this->idHash = $result[0]->idhash;
-			$this->userId = $result[0]->userid;
-			$this->lastActive = $result[0]->lastactivity;
-
-			return ($this->idHash == $newIdHash && (time() - $this->lastActive) < 900 ? $this->userId : false);
-		}
-
-		return false;
-	}
-
-	private function getCookie($id, $pass)
-	{
-		$result = DB::connection($this->connection)->table('user')->where('userid', '=', $id)->take(1)->get();
-		if($result)
-		{
-			$result = DB::connection($this->connection)->table('user')->where('userid', '=', $id)->take(1)->get();
-
-			if ($result)
-			{
-				$dbpass = $result[0]->password;
-				
-				if (md5($dbpass . $this->cookieHash) == $pass)
+				if(!$userinfo)
 				{
-					return $id;
+					return false;
 				}
+
+				$userinfo = $userinfo[0];
+
+				$userinfo->sessionhash = $session->sessionhash;
+
+				$this->setUserInfo($userinfo);
+
+				$updateSession = array(
+					'lastactivity' => time(),
+					'location' => Request::server('REQUEST_URI'),
+				);
+
+				DB::connection($this->connection)->table($this->databasePrefix . 'session')->where('sessionhash', '=', $userinfo->sessionhash)->update($updateSession);
+
+				return true;
 			}
-			return false;
 		}
+
+		return false;
+	}
+
+	protected function isValidCookieUser($userid, $password)
+	{
+		$dbPass = DB::connection($this->connection)->table($this->databasePrefix . 'user')->where('userid', '=', $userid)->take(1)->get(array('password'));
+		if($dbPass)
+			$dbPass = $dbPass[0];
+
+		if($password == md5($dbPass->password . $this->cookieHash))
+			return intval($userid);
+		
+		return false;
+	}
+
+	protected function isValidLogin($username, $password)
+	{
+		$res = DB::connection($this->connection)->table($this->databasePrefix . 'user')->where('username', '=', $username)
+			->where('password', '=', 'md5(concat(md5(' . $password . '), salt))')->take(1)->get();
+		if($res)
+			return intval($res[0]->userid);
+
+		return false;
+	}
+
+	protected function createCookieUser($userid, $passwordd)
+	{
+		setcookie($this->cookiePrefix . 'userid', $userid, time() + 31536000, '/');
+		setcookie($this->cookiePrefix . 'password', md5($password . $this->cookieHash), time() + 31536000, '/');
+	}
+
+	protected function createSession($userid)
+	{
+		$hash = md5(microtime() . $userid . Request::server('REMOTE_ADDR'));
+
+		$timeout = time() + $this->cookieTimeout;
+
+		setcookie($this->cookiePrefix . 'sessionhash', $hash, $timeout, '/');
+
+		$session = array (
+			'userid' => $userid,
+			'sessionhash' => $hash,
+			'host' => Request::server('REMOTE_ADDR'),
+			'idhash' => $this->fetchIdHash(),
+			'lastactivity' => time(),
+			'location' => Request::server('REQUEST_URI'),
+			'useragent' => Request::server('HTTP_USER_AGENT'),
+			'loggedin' => 1,
+		);
+
+		DB::connection($this->connection)->table($this->databasePrefix . 'session')->insert($session);
+
+		return $hash;
+	}
+
+	protected function deleteSession()
+	{
+		setcookie($this->cookie_prefix . 'sessionhash', '', time() - 3600);
+		setcookie($this->cookie_prefix . 'userid', '', time() - 3600);
+		setcookie($this->cookie_prefix . 'password', '', time() - 3600);
+
+		DB::connection($this->connection)->table($this->databasePrefix . 'session')->where('sessionhash', '=', $this->userInfo->sessionHash)->delete();
+	}
+
+	protected function fetchIdHash()
+	{
+		return md5(Request::server('HTTP_USER_AGENT') . $this->fetchIp());
+	}
+
+	protected function fetchIp()
+	{
+		$ip = Request::server('REMOTE_ADDR');
+		return implode('.', array_slice(explode('.', $ip), 0, 4 -1));
+	}
+
+	protected function setUserInfo($userinfo)
+	{
+		$this->userInfo = (object) $userinfo;
 	}
 
 }
--- src/config/config.php
+++ src/config/config.php
@@ -4,6 +4,22 @@ return array(
 	'connection' => 'vbdb',
 	'cookie_hash' => 'WL0S0rb5UvsDNjwfyqRhQM5CtG',
 	'cookie_prefix' => 'bb_',
-	'forum_path' => 'http://unkso.dev/',
-	'user_groups' => array(),
+	'db_prefix' => '',
+	'forum_path' => 'http://unkso.dev/barracks',
+	'user_groups' => array(
+		'admin' => array(6),
+		'moderator' => array(5, 7),
+		'user' => array(2),
+		'banned' => array(8),
+		'guest' => array(3),
+		'us members' => array(92),
+	),
+	'user_columns' => array(
+		'userid',
+		'username',
+		'usergroupid',
+		'membergroupids',
+		'email',
+		'salt'
+	),
 );
