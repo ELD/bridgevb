4d82f83d612300677835259e4c2f08b8660f75be
Eric <eric.dattore@gmail.com>
2013-05-31 20:40:35 -0600

    Made the config file more generic

--- src/config/config.php
+++ src/config/config.php
@@ -1,11 +1,11 @@
 <?php
 
 return array(
-	'connection' => 'vbdb',
-	'cookie_hash' => 'WL0S0rb5UvsDNjwfyqRhQM5CtG',
+	'connection' => 'mysql',
+	'cookie_hash' => 'AdflkjEr90234asdlkj1349SDFkl',
 	'cookie_prefix' => 'bb_',
-	'db_prefix' => '',
-	'forum_path' => 'http://unkso.dev/barracks',
+	'db_prefix' => 'vb_',
+	'forum_path' => 'http://example.com/',
 	'user_groups' => array(
 		'Admin' => array(6),
 		'Moderator' => array(7),
@@ -13,7 +13,6 @@ return array(
 		'User' => array(2),
 		'Banned' => array(8),
 		'Guest' => array(3),
-		'=US= Members' => array(92),
 	),
 	'user_columns' => array(
 		'userid',
